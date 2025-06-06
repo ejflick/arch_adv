68105f77874d36139a75e493720646aa25196982
thebraveparrot <115940736+thebraveparrot@users.noreply.github.com>
2025-06-06 22:03:40 +0800

    Remove build folder

--- build/conf.lua
+++
@@ -1,8 +0,0 @@
-love.conf = function(t)
-  t.window.width = 800
-  t.window.height = 640
-  t.window.title = "Archaeology Adventure"
-  t.modules.joystick = false
-  t.modules.physics = false
-  t.console = true
-end
